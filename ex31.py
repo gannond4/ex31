f302d9eee71d11349f4d9ced0f8c159a87882ea0
David <gannond@hartwick.edu>
2019-10-15 08:32:36 -0400

    Update

--- ex31.py
+++ ex31.py
@@ -1,1 +1,40 @@
-print("Hello World")
+print("""You enter a dark room with two doors.
+Do you go through door #1 or door #2""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake.")
+    print(" What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("the bear eats your face off. Good job")
+    elif bear == "2"
+        print("The bear eats your leg off. good job")
+    else:
+        print(f"well, doing {bear} is probably better")
+        print("bear runs away")
+
+elif door == "2":
+    print("1. blueberries.")
+    print("2. Yellow Jacket clothespins")
+    print("3. understanding revolvers yelling melodies")
+
+
+    insanity = input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello")
+        print("Good Job")
+    else:
+        print("The insanity rots your eyes into a pool of muck")
+        print("Good Job!")
+
+else:
+    print("You stumble around and fall on a knife and die. Good job!")
+
+
